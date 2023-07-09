a3bfc93e511e9892d1a373656c71032305c8fda1
S-Kenshin <ps0139hk@gmail.com>
2023-07-09 20:27:09 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './typeorm/entities/User';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './src/users/controllers/users/users.controller';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST, // 環境変数から値を取得する
+      port: 3306, // 数値として解釈するためにparseIntを使用
+      username: 'test',
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService],
+})
+export class AppModule {}
